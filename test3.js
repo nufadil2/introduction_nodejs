3ea05c2caa67ee9a458c772e2c32aee81e23f707
nufadil2 <100058686+nufadil2@users.noreply.github.com>
2022-05-07 15:48:57 +0700

    Add files via upload

---
+++ test3.js
@@ -0,0 +1,44 @@
+// Ini comment satu baris
+/* 
+Tipe data di JavaScript adalah: 
+1. string (teks), 
+2. integer atau number (bilangan bulat), 
+3. float(bilangan pecahan), 
+4. boolean, 
+5. object
+*/
+
+//Tipe data string
+const namaDepan = "Nurul";
+const namaBelakang= 'Fadil';
+let alamat = 'jalan kampung jati';
+
+
+// Tipe data number di JavaScript
+let umur = 25;
+const tangglLahir = 12;
+const bulanLahir = 2;
+
+// tipe data float
+let beratBadan = 61.9;
+let tinggiBadan = 159.0;
+let nilaiBMI = 41.5;
+
+const angkaPertama = 2020;
+const angkaKedua = 8;
+
+//penjumlahan
+total = angkaPertama + angkaKedua;
+console.log(total);
+
+//pengurangan
+total = angkaPertama - angkaKedua;
+console.log(total);
+
+//pembagian
+total = angkaPertama / angkaKedua;
+console.log(total);
+
+//perkalian
+total = angkaPertama * angkaKedua;
+console.log(total);
